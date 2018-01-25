6faa10d1caa34fb1f7dd20a1ec6ea6b245910c2d
huangegege <330673934@qq.com>
2018-01-25 16:16:55 +0800

    cookie config

--- src/main/java/com/mmall/util/CookieUtil.java
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -13,8 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class CookieUtil {
 
-    private final static String COOKIE_DOMAIN = ".happymmall.com";
-    private final static String COOKIE_NAME = "mmall_login_token";
+    private final static String COOKIE_DOMAIN = PropertiesUtil.getProperty("cookie.domain",".huanliu.org");
+    private final static String COOKIE_NAME = PropertiesUtil.getProperty("cookie.login.token.name","mmall_login_token");
 
     public static String readLoginToken(HttpServletRequest request){
         Cookie[] cks = request.getCookies();
